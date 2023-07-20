439924ca5c5d2839327271c09f5c196caf6210c4
shobhanoffl <shobhankumar.20ads@sonatech.ac.in>
2023-07-20 07:56:25 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "malayalaM";
+        char[] charArray = str.toCharArray();
+        int left = 0;
+        int right = charArray.length - 1;
+
+        while (left < right) {
+            // Swap characters at left and right pointers
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+
+            // Move the pointers towards the center
+            left++;
+            right--;
+        }
+        System.out.println(new String(charArray));
+    }
+}
